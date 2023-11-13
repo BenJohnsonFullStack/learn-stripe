4ed283fdf0d46039fa8ed6f2e1bef0cf5de969e2
Ben Johnson <monk.sound.design@gmail.com>
2023-11-13 12:02:15 -0500

    product cards

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,17 @@
 import { Row, Col } from "react-bootstrap";
 import { products } from "@/constants";
+import ProductCard from "@/components/ProductCard";
 
 export default function Home() {
   return (
     <>
-      <h1>Welcome to the Store.</h1>
-      <Row xs={1} md={3} className="g-4">
+      <h1 align="center" className="p-3">
+        Welcome to the Store.
+      </h1>
+      <Row xs={1} md={3} className="g-4 mt-4">
         {products.map((product) => (
           <Col align="center" key={product.id}>
-            <h2>{product.title}</h2>
+            <ProductCard {...product} />
           </Col>
         ))}
       </Row>
---
+++ components/ProductCard.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Card, Button, Row, Col, Form } from "react-bootstrap";
+
+const ProductCard = ({ title, price }) => {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>${price}</Card.Text>
+        <Button variant="primary">Add to Cart</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProductCard;
